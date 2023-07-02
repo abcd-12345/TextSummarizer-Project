dd4aab19aadf69ef8c46dccbee969048f0736587
abcd-12345 <abcd_12345_abcd_12345@yahoo.com>
2023-07-02 19:56:42 -0400

    logging test

--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -7,7 +7,9 @@ log_dir ="logs"
 log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
-logging.basicConfig(level=logging.INFO, format=logging_str
+logging.basicConfig(
+    level=logging.INFO, 
+    format=logging_str,
 handlers=[
     logging.FileHandler(log_filepath),
     logging.StreamHandler(sys.stdout)
